77000f777593e20df4cec7b90fb3f6c91875b3f7
Steffen Pauer <steffen.pauer@siemens.com>
2017-03-20 11:54:48 +0100

    red

--- 4domovea/imagemagick.bat
+++ 4domovea/imagemagick.bat
@@ -4,8 +4,9 @@ REM ******** domoveaStyle ******
 REM forfiles /p D:\zzz\knx-uf-iconset\raw_480x480 /m *.png /c "cmd /c magick D:\zzz\knx-uf-iconset\4domovea\_backgrounds\bg_domo.png @file -gravity center -composite D:\zzz\knx-uf-iconset\4domovea\_domovea\domostyle_@file"
 
 REM ******** foreground-black background-white******
-forfiles /p D:\zzz\knx-uf-iconset\raw_480x480 /m *.png /c "cmd /c magick @file -fill black -opaque white -blur 0x1 D:\zzz\knx-uf-iconset\4domovea\_black\black_@file"
+REM forfiles /p D:\zzz\knx-uf-iconset\raw_480x480 /m *.png /c "cmd /c magick @file -fill black -opaque white -blur 0x1 D:\zzz\knx-uf-iconset\4domovea\_black\black_@file"
 
-
+REM ******** foreground-red background-white******
+forfiles /p D:\zzz\knx-uf-iconset\raw_480x480 /m *.png /c "cmd /c magick @file -fill red -opaque white -blur 0x1 D:\zzz\knx-uf-iconset\4domovea\_red\red_@file"
 
 
